af3f1d352c51be7016df67c0a85b1985b64b7cac
Sushant-Singh-Rathore <ssr7705@gmail.com>
2020-05-23 23:21:46 +0530

    SignIn & SignUp UI

---
+++ app/src/main/java/com/example/navigusdocs/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.navigusdocs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OpenSignupPage(View view) {
+        startActivity(new Intent(MainActivity.this,SignupActivity.class));
+    }
+}
